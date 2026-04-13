2b049b349903afd15b2de53b7e0961fbe2bd77bd
jefersonleon <jefersonleonblue@gmail.com>
2026-04-13 20:33:25 -0300

    Projeto CRUD MVC - 13/04/2025 Adicionei o driver de conexão, foi criado  a classe ConnectionFctory e criuaiado o arquivo db.properied

---
+++ src/br/ulbra/dao/ConnectionFactory.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ulbra.dao;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+/**
+ *
+ * @author qijef
+ */
+public class ConnectionFactory {
+    private static String url;
+    private static String user;
+    private static String password;
+
+    static {
+        try {
+            Properties props = new Properties();
+
+            InputStream input = ConnectionFactory.class
+                    .getClassLoader()
+                    .getResourceAsStream("db.properties");
+
+            props.load(input);
+
+            url = props.getProperty("db.url");
+            user = props.getProperty("db.user");
+            password = props.getProperty("db.password");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao carregar propriedades");
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro na conexão", e);
+        }
+    }
+
+
+}
